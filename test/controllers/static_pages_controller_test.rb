e462f70a2c95b099e32ab93090aa0ada80885f81
rennrennrenn <rene@railslove.com>
2018-01-22 12:18:19 +0100

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-  end
+  end 
 
   test "should get home" do
     get static_pages_home_url
